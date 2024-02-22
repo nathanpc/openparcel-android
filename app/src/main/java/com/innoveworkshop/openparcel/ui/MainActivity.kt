71525a1bac9f1f64f0a5c669b3aa7c5797ec704d
Nathan Campos <nathan@innoveworkshop.com>
2024-02-22 16:27:35 +0000

    Updated Composable layouts with new Parcel model
    Updated and fully implemented Parcel model was integrated into the
    Composable layout classes.

--- app/src/main/java/com/innoveworkshop/openparcel/ui/MainActivity.kt
+++ app/src/main/java/com/innoveworkshop/openparcel/ui/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
@@ -18,18 +19,24 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.innoveworkshop.openparcel.R
+import com.innoveworkshop.openparcel.model.Carrier
+import com.innoveworkshop.openparcel.model.Parcel
+import com.innoveworkshop.openparcel.model.ParcelUpdate
 import com.innoveworkshop.openparcel.ui.theme.OpenparcelTheme
+import com.innoveworkshop.openparcel.utils.DateUtil
+import java.net.URI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainView()
+                    MainView(parcels = listOf())
                 }
             }
         }
@@ -50,7 +57,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainView() {
+fun MainView(parcels: List<Parcel>) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
     Scaffold (
@@ -88,20 +95,14 @@ fun MainView() {
             )
         },
     ) { innerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text =
-                """
-                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
-
-                    It also contains some basic inner content, such as this text.
-                """.trimIndent(),
-            )
+            items(parcels) { parcel ->
+                ParcelCard(parcel = parcel)
+            }
         }
     }
 }
@@ -110,6 +111,66 @@ fun MainView() {
 @Composable
 fun MainViewPreview() {
     OpenparcelTheme {
-        MainView()
+        MainView(
+            parcels = listOf(
+                Parcel(
+                    id = 0,
+                    name = "Panasonic Lumix DMC-GF1",
+                    delivered = false,
+                    carrier = Carrier(
+                        id = "ctt",
+                        name = "CTT"
+                    ),
+                    color = Color(0xFFDE0024).toArgb(),
+                    trackingCode = "CA767344619DE",
+                    trackingUrl = URI.create("https://www.dhl.com/pt-pt/home/tracking.html?tracking-id=CA767344619DE&submit=1"),
+                    trackingHistory = arrayOf(
+                        ParcelUpdate(
+                            title = "Order data transmitted electronically",
+                            timestamp = DateUtil.fromISO8601("2024-02-19T07:29:00.000Z")
+                        )
+                    ),
+                    lastUpdated = DateUtil.fromISO8601("2024-02-19T07:29:00.000Z")
+                ),
+                Parcel(
+                    id = 0,
+                    name = "Panasonic Lumix DMC-GF1",
+                    delivered = false,
+                    carrier = Carrier(
+                        id = "ctt",
+                        name = "CTT"
+                    ),
+                    color = Color(0xFFFFCC00).toArgb(),
+                    trackingCode = "CA767344619DE",
+                    trackingUrl = URI.create("https://www.dhl.com/pt-pt/home/tracking.html?tracking-id=CA767344619DE&submit=1"),
+                    trackingHistory = arrayOf(
+                        ParcelUpdate(
+                            title = "Order data transmitted electronically",
+                            timestamp = DateUtil.fromISO8601("2024-02-19T07:29:00.000Z")
+                        )
+                    ),
+                    lastUpdated = DateUtil.fromISO8601("2024-02-19T07:29:00.000Z")
+                ),
+                Parcel(
+                    id = 0,
+                    name = "Panasonic Lumix DMC-GF1",
+                    delivered = false,
+                    carrier = Carrier(
+                        id = "ctt",
+                        name = "CTT"
+                    ),
+                    color = Color(0xFFDC1332).toArgb(),
+                    trackingCode = "CA767344619DE",
+                    trackingUrl = URI.create("https://www.dhl.com/pt-pt/home/tracking.html?tracking-id=CA767344619DE&submit=1"),
+                    trackingHistory = arrayOf(
+                        ParcelUpdate(
+                            title = "Order data transmitted electronically",
+                            timestamp = DateUtil.fromISO8601("2024-02-19T07:29:00.000Z")
+                        )
+                    ),
+                    lastUpdated = DateUtil.fromISO8601("2024-02-19T07:29:00.000Z")
+                )
+            )
+        )
     }
 }
