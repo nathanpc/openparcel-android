2f9d44f67b34d19b494be7a7ea4e2ff273e679f9
Nathan Campos <nathan@innoveworkshop.com>
2024-02-24 19:00:19 +0000

    Added a utility function to check if dates are different
    Reformatted the DateUtil class and introduced a function to check if
    dates (not the full timestamp) are different.

--- app/src/main/java/com/innoveworkshop/openparcel/utils/DateUtil.kt
+++ app/src/main/java/com/innoveworkshop/openparcel/utils/DateUtil.kt
@@ -1,9 +1,12 @@
 package com.innoveworkshop.openparcel.utils
 
+import android.content.Context
 import android.text.format.DateUtils
 import java.time.Instant
 import java.time.format.DateTimeFormatter
+import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 
 /**
  * A collection of simple utilities for working with dates.
@@ -17,9 +20,8 @@ class DateUtil {
          *
          * @return Date object.
          */
-        fun fromISO8601(dateStr: String): Date {
-            return Date.from(Instant.from(DateTimeFormatter.ISO_INSTANT.parse(dateStr)))
-        }
+        fun fromISO8601(dateStr: String): Date =
+            Date.from(Instant.from(DateTimeFormatter.ISO_INSTANT.parse(dateStr)))
 
         /**
          * Gets a human-readable representation of a relative timeframe.
@@ -29,9 +31,33 @@ class DateUtil {
          *
          * @return A human-readable representation of a relative timeframe.
          */
-        fun getRelativeTimeString(date: Date, reference: Date = Date.from(Instant.now())): String {
-            return DateUtils.getRelativeTimeSpanString(
-                date.time, reference.time, 0).toString()
+        fun getRelativeTimeString(date: Date, reference: Date = Date.from(Instant.now())): String =
+            DateUtils.getRelativeTimeSpanString(date.time, reference.time, 0).toString()
+
+        /**
+         * Gets the current default locale set by the user.
+         *
+         * @param context Current application context.
+         *
+         * @return The current default locale set by the user.
+         */
+        fun getLocale(context: Context): Locale = context.resources.configuration.locales[0]
+
+        /**
+         * Checks if a date is different from another.
+         *
+         * @param calendar Calendar object to compare with.
+         * @param date     Date object to compare with.
+         *
+         * @return True if the day, month, or year are different between the two objects.
+         */
+        fun isDateDifferent(calendar: Calendar, date: Date): Boolean {
+            val other: Calendar = Calendar.getInstance()
+            other.time = date
+
+            return (calendar.get(Calendar.DAY_OF_MONTH) != other.get(Calendar.DAY_OF_MONTH)) or
+                    (calendar.get(Calendar.MONTH) != other.get(Calendar.MONTH)) or
+                    (calendar.get(Calendar.YEAR) != other.get(Calendar.YEAR))
         }
     }
 }
